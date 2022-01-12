bbcfe98f5deeac2759b9cbbfd8b252bb3a48a69f
Eliana <elianacalote@gmail.com>
2022-01-12 22:25:40 +0000

    message asking input working

--- form/src/App.js
+++ form/src/App.js
@@ -7,41 +7,61 @@ function App() {
     lastName:"",
     email:"",
   });
-
+  
+  const [submitted, setSubmitted] = useState(false);
+  
   const handleFirstNameInputChange = (event) => {
     console.log(handleFirstNameInputChange)
     setInput({...value, firstName: event.target.value})
   }
 
   const handleLAstNameInputChange = (event) => {
+    console.log(handleLAstNameInputChange)
     setInput({...value, lastName: event.target.value})
   }
 
   const handleemailInputChange = (event) => {
     setInput({...value, email: event.target.value})
   }
-
+  
+  const handleSubmit = (event) =>{
+  event.preventDefault();
+  setSubmitted(true);
+  }
 
 
   return (
     <div className="form-container">
-    <form className="register-form">
-       <input onChange={handleFirstNameInputChange}
+    <form className="register-form" onSubmit={handleSubmit}>
+    {submitted ? <div className="success-message">Success!Thank you for registering</div> :null}
+       <input 
+       disabled={submitted}
+        onChange={handleFirstNameInputChange}
         value = {value.firstName}
         className="form-container"
         placeholder="First Name"
         name="firstName"/>
-         <input onChange={handleLAstNameInputChange}
+        {!submitted && !value.firstName ?<span>Please enter a first name</span> :null}
+        
+        <input 
+        disabled={submitted}
+         onChange={handleLAstNameInputChange}
          value= {value.lastName}
-        className="form-container"
-        placeholder="Last Name"
-        name="lastName"/>
-         <input onChange={handleemailInputChange}
+         className="form-container"
+         placeholder="Last Name"
+         name="lastName"/>
+         {!submitted && !value.lastName ? <span>please enter a last name</span> : null}
+        <input 
+        
+        disabled={submitted}
+         onChange={handleemailInputChange}
          value= {value.email}
-        className="form-container"
-        placeholder="Email"
-        name="email"/>
+         className="form-container"
+         placeholder="Email"
+         name="email"/>
+         {!submitted && !value.email ? <span>Please enter a email</span> : null}
         </form>
+        <button>Submit</button>
     </div>
   
   );
